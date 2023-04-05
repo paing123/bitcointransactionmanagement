ad1c214ab14008a45aeec271ffa709f91e37311c
xitm-ppp <pyaephyopaing@xitmyanmar.com>
2023-04-05 15:29:08 +0630

    remove all file and update source file

--- src/main/java/com/anymindgroup/bitcoinmanagement/service/TransactionCommandService.java
+++
@@ -1,9 +0,0 @@
-package com.anymindgroup.bitcoinmanagement.service;
-
-import com.anymindgroup.bitcoinmanagement.dto.TransactionDto;
-
-public interface TransactionCommandService {
-    
-   void save(TransactionDto transaction) throws Exception;
-    
-}
